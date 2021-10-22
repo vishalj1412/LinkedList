a83608c1f389fbbba3379719304c026bf6b991c9
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-10-22 16:15:59 +0530

    Added: Insert element in linked list

---
+++ MyLinedList.java
@@ -0,0 +1,54 @@
+package com.bl.javapractice;
+
+public class MyLinkedList {
+    public INode head;
+    public INode tail;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void add(INode myNode) {
+        if (this.tail == null) {
+            this.tail = myNode;
+        }
+        if (this.head == null) {
+            this.head = myNode;
+        } else {
+            INode tempNode = this.head;
+            this.head = myNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    public void printMyNodes(){
+        StringBuffer myNodes =new StringBuffer("My Nodes: ");
+        INode tempNode = head;
+        while (tempNode.getNext() != null){
+            myNodes.append(tempNode.getKey());
+            if (!tempNode.equals(tail)) myNodes.append("->");
+            tempNode = tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+    public void append(INode myNode) {
+        if (this.head == null) {
+            this.head = myNode;
+        }
+        if (this.tail == null) {
+            this.tail = myNode;
+        } else {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
+
+        }
+    }
+
+    public void insert(INode myNode,INode newNode){
+        INode tempNode = myNode.getNext();
+        myNode.setNext(newNode);
+        newNode.setNext(tempNode);
+
+    }
+}
